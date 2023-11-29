db9d25597992b82cf12e869bd223963efa78b72c
chobo5 <wnsdus1008@gmail.com>
2023-11-29 17:57:56 +0900

    메서드로 추출하기

--- myapp/src/bitcamp/myapp/MyApp.java
+++ myapp/src/bitcamp/myapp/MyApp.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 public class MyApp {
     private static final String RESET = "\u001B[0m";
     private static final String FONT_RED_BOLD = "\033[1;31m";
-    private static final String[] menuArray = {"[과제관리 시스템]", "1.과제", "2.게시글", "3.도움말", "4.종료"};
+    private static final String[] MENUS = {"[과제관리 시스템]", "1.과제", "2.게시글", "3.도움말", "4.종료"};
     private static final Scanner sc = new Scanner(System.in);
 
     private static void showMenu() {
-        System.out.println(FONT_RED_BOLD + menuArray[0] + RESET);
+        System.out.println(FONT_RED_BOLD + MENUS[0] + RESET);
         System.out.println();
-        System.out.println();
-        for (int i = 1; i < menuArray.length ; i++) {
-            System.out.println(menuArray[i]);
+        for (int i = 1; i < MENUS.length ; i++) {
+            System.out.println(MENUS[i]);
         }
     }
+
+    private static String getUserInput() {
+        System.out.print("> ");
+        return MyApp.sc.nextLine();
+    }
+
     public static void main(String[] args) {
 
         showMenu();
         boolean run = true;
         while (run) {
-            System.out.print("> ");
-            String input = sc.nextLine();
+            String input = getUserInput();
             switch (input) {
                 case "1":
                     System.out.println("과제입니다.");
@@ -45,9 +49,5 @@ public class MyApp {
                     break;
             }
         }
-
-
-
-
     }
 }
