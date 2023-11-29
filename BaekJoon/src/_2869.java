d13c9647a039db08069a067ab230956cbe66a4ec
chobo5 <wnsdus1008@gmail.com>
2023-11-29 16:50:30 +0900

    not solved yet

--- BaekJoon/src/_2869.java
+++ BaekJoon/src/_2869.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class _2869 {
+import java.util.Scanner;
+
+public class _2869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int v = sc.nextInt();
+        int day = 0;
+        sc.close();
+        int dayDistance = a - b;
+        int minusLastDay = v - a;
+
+        if (dayDistance > minusLastDay) {
+            day = minusLastDay / dayDistance + 2;
+        } else {
+            day = minusLastDay / dayDistance + 1;
+        }
+        System.out.println(day);
+
+
+    }
 }
