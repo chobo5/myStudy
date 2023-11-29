d38b18c02f22f7dd82a60053b1ba80c0a94f6582
chobo5 <wnsdus1008@gmail.com>
2023-11-29 09:54:11 +0900

    baekjoon 2292

--- BaekJoon/src/_2292.java
+++ BaekJoon/src/_2292.java
@@ -1,2 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class _2292 {
+    public static void main(String[] args) throws IOException {
+        //1 1개  1
+        // 2 3 4 5 6 7 6개   2
+        // 8 9 10 11 12 13 14 15 16 17 18 19 12개    3
+        // 20 ~ 37 18개  4
+        // 38 ~ 61 24개  5
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int x = 0;
+        while (n > 0) {
+            n = n - (6 * x);
+            System.out.println("n은" + n);
+            x++;
+            if (n == 1) break;
+            System.out.println("x는" + x);
+        }
+        System.out.print(x);
+
+
+    }
 }
