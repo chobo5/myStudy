e2520a114ab856b8044f1623aa872712e1b87c94
chobo5 <wnsdus1008@gmail.com>
2023-12-04 13:51:40 +0900

    baekjoon 2750

--- BaekJoon/src/_2750.java
+++ BaekJoon/src/_2750.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class _2750 {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class _2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sc.nextLine();
+        }
+        sc.close();
+
+        //bubble sort
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i ; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int x : arr) {
+            System.out.println(x);
+        }
+
+
+
+    }
 }
