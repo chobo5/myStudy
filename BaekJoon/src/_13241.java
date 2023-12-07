5ddaec94e8ed9db206ea86d81612bc6831655b44
chobo5 <wnsdus1008@gmail.com>
2023-12-07 10:33:29 +0900

    baekjoon 13241

--- BaekJoon/src/_13241.java
+++ BaekJoon/src/_13241.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class _13241 {
+import java.util.Scanner;
+
+public class _13241 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        long b = sc.nextLong();
+        sc.close();
+        System.out.println(a * b / gcd(a, b));
+
+
+    }
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
 }
