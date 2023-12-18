42d7741f018ac244ff0bbbfff68efaf6dd036589
chobo5 <wnsdus1008@gmail.com>
2023-12-18 16:43:20 +0900

    baekjoon 10951

--- Algorithm/BaekJoon/src/_10951.java
+++ Algorithm/BaekJoon/src/_10951.java
@@ -1,11 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class _10951 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        boolean run = true;
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String line;
+        while ((line=br.readLine()) != null) {
+            st = new StringTokenizer(line, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+        System.out.println(sb);
 
     }
 }
