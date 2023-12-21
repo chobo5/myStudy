3cf38597ab1e12f2b4fcd01048917d9c5ec7e40f
chobo5 <wnsdus1008@gmail.com>
2023-12-21 19:26:11 +0900

    Java I/O 간단한 Decorator Pattern

---
+++ DesignPattern/DecoratorPattern/JavaIODacorator/src/LowerCaseInputStream.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class LowerCaseInputStream extends FilterInputStream {
+
+    public LowerCaseInputStream(InputStream in) {
+        super(in);
+    }
+
+    @Override
+    public int read() throws IOException {
+        int c = in.read();
+        return (c == -1 ? c : Character.toLowerCase((char) c));
+
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        int result = in.read(b, off, len);
+        for (int i = off; i < off + result ; i++) {
+            b[i] = (byte) Character.toLowerCase((char) b[i]);
+        }
+        return result;
+    }
+}
