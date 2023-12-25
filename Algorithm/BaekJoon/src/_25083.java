824c17a0df48868a87a03be5b7dcc603ea149c0e
chobo5 <wnsdus1008@gmail.com>
2023-12-25 19:13:57 +0900

    문자열

---
+++ Algorithm/BaekJoon/src/_25083.java
@@ -0,0 +1,13 @@
+public class _25083 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+
+
+}
