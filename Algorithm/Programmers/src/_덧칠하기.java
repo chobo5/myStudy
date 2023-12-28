e06de144ed753f673809c71277df21038a34579d
chobo5 <wnsdus1008@gmail.com>
2023-12-28 18:50:07 +0900

    programmers 덧칠하기

--- Algorithm/Programmers/src/_덧칠하기.java
+++ Algorithm/Programmers/src/_덧칠하기.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class _덧칠하기 {
+public class _덧칠하기 {
+    public int solution(int n, int m, int[] section) {
+        int start = section[0];
+        int end = section[0] + m - 1;
+        int answer = 1;
+        for(int i = 0; i < section.length; i++) {
+            if (section[i] >= start && section[i] <= end) {
+                continue;
+            } else {
+                start = section[i];
+                end = section[i] + (m - 1);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
 }
