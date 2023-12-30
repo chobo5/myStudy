12a165457e69f184319c167c6a2f89494856f558
chobo5 <wnsdus1008@gmail.com>
2023-12-30 16:11:24 +0900

    programmers 과일장수

---
+++ Algorithm/Programmers/src/_과일장수.java
@@ -0,0 +1,33 @@
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class _과일장수 {
+
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int k, int m, int[] score) {
+                int answer = 0;
+                int[] scoreArr = score;
+                Arrays.sort(scoreArr);
+                int count = 0;
+                for (int i = 0; i < score.length / m; i++) {
+                    int price = score[score.length % m + (m * count++)];
+                    answer += price * m;
+                }
+                return answer;
+            }
+        }
+        int[] arr = {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2};
+        Solution solution = new Solution();
+        System.out.println(solution.solution(4, 3, arr));
+    }
+
+
+}
+
+
+
+
