61f316cd9a45f31b0d5673193f598ea15a304cac
chobo5 <wnsdus1008@gmail.com>
2024-01-02 19:11:43 +0900

    programmers 뒤에있는큰수찾기

---
+++ Algorithm/Programmers/src/_뒤에있는큰수찾기.java
@@ -0,0 +1,20 @@
+public class _뒤에있는큰수찾기 {
+    public static void main(String[] args) {
+        class Solution {
+            public int[] solution(int[] numbers) {
+                int[] answer = new int[numbers.length];
+                for (int i = 0; i < numbers.length; i++) {
+                    for (int j = i; j < numbers.length; j++) {
+                        if (numbers[j] > numbers[i]) {
+                            answer[i] = numbers[j];
+                            break;
+                        } else {
+                            answer[i] = -1;
+                        }
+                    }
+                }
+                return answer;
+            }
+        }
+    }
+}
