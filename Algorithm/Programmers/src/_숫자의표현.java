b16348d0e3241c1e1562b71c897dc54df296ef92
chobo5 <wnsdus1008@gmail.com>
2024-01-04 15:56:06 +0900

    programmers 숫자의 표현

---
+++ Algorithm/Programmers/src/_숫자의표현.java
@@ -0,0 +1,23 @@
+public class _숫자의표현 {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int n) {
+                int answer = 0;
+                for (int i = 1; i <= n ; i++) {
+                    int sum = 0;
+                    for (int j = i; j <= n; j++) {
+                        sum += j;
+                        if (sum == n) {
+                            answer += 1;
+                            break;
+                        }
+                        if (sum > n) {
+                            break;
+                        }
+                    }
+                }
+                return answer;
+            }
+        }
+    }
+}
