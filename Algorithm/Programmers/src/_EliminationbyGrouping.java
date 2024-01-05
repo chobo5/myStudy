9e77ff42948ff3799fccf56265d956b5a5455b34
chobo5 <wnsdus1008@gmail.com>
2024-01-05 19:34:17 +0900

    programmers 짝지어 제거하

--- Algorithm/Programmers/src/_EliminationbyGrouping.java
+++ Algorithm/Programmers/src/_EliminationbyGrouping.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class _EliminationbyGrouping {
+import java.util.Stack;
+
+public class _EliminationbyGrouping {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(String s) {
+                Stack<Character> stack = new Stack<>();
+                for (int i = 0; i < s.length(); i++) {
+                    char c = s.charAt(i);
+                    if (!stack.empty()) {
+                        if (stack.peek().equals(c)) {
+                            stack.pop();
+                        } else {
+                            stack.push(c);
+                        }
+                    } else {
+                        stack.push(c);
+                    }
+                }
+
+                if (stack.empty()) {
+                    return 1;
+                }else {
+                    return 0;
+                }
+            }
+        }
+        System.out.println(new Solution().solution("cdcd"));
+    }
 }
