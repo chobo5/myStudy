540cf40b2f4fa8d6333048066761ab05a9a77571
chobo5 <wnsdus1008@gmail.com>
2024-01-18 19:14:47 +0900

    IO

--- This_is_Java/src/io/Ex1.java
+++ This_is_Java/src/io/Ex1.java
@@ -1,2 +1,46 @@
-package io;public class Ex1 {
+package io;
+
+import java.io.*;
+
+public class Ex1 {
+  public static void main(String[] args) throws Exception {
+//    File file = new File("temp");
+//    file.mkdir();
+
+    FileOutputStream out = new FileOutputStream("temp/test.txt");
+    byte a = 12;
+    byte b = 13;
+    byte c = 14;
+    byte d = 15;
+    byte e = 16;
+
+    byte[] bytes = {20, 30, 40, 50, 60, 70};
+
+    out.write(a);
+    out.write(b);
+    out.write(c);
+    out.write(d);
+    out.write(e);
+
+    out.write(bytes);
+//    out.write(bytes, 0, 3);
+
+
+
+    out.close();
+    FileInputStream in = new FileInputStream("temp/test.txt");
+    int i;
+    while ((i = in.read()) != -1) {
+      System.out.println(i);
+    }
+//    byte[] buf = new byte[3];
+//    int count = in.read(buf);
+//    for (int i = 0; i < count; i++) {
+//      System.out.println(buf[i]);
+//    }
+
+
+    in.close();
+
+  }
 }
--- This_is_Java/src/io/Ex2.java
+++ This_is_Java/src/io/Ex2.java
@@ -1,2 +1,49 @@
-package io;public class Ex2 {
+package io;
+
+import java.io.*;
+
+public class Ex2 {
+  public static void main(String[] args) throws IOException {
+    Writer writer = new BufferedWriter(new FileWriter("temp/test2.txt"));
+    char c1 = '이';
+    char c2 = 'j';
+
+    char[] cs = {'그', '러', '나', 'b', 'u', 't'};
+    writer.write(c1);
+    writer.write(c2);
+    writer.write(cs);
+    writer.flush();
+    writer.close();
+
+
+    Reader reader = new BufferedReader(new FileReader("temp/test2.txt"));
+    int i;
+    while ((i = reader.read()) != -1) {
+      System.out.println((char) i);
+    }
+    reader.close();
+
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    String str;
+//    while (!(str = br.readLine()).equals("-1")) {
+//      System.out.println(str);
+//    }
+//    br.close();
+
+    DataOutputStream out = new DataOutputStream(new FileOutputStream("./temp/test3.txt"));
+    out.writeUTF("안녕하세요");
+    out.writeUTF("하지만");
+    out.writeUTF("however");
+    out.writeUTF("nevertheless");
+    out.flush();
+    out.close();
+
+    DataInputStream in = new DataInputStream(new FileInputStream("./temp/test3.txt"));
+    for (int j = 0; j < 4 ; j++) {
+      System.out.println(in.readUTF());
+    }
+
+    
+
+  }
 }
