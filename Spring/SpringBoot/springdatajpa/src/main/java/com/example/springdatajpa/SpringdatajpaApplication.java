69c7a45fd204124b81011a47c6e60ac1bf196d19
chobo5 <wnsdus1008@gmail.com>
2024-01-22 21:03:37 +0900

    12- SQL부터 Spring Data JPA까지

--- Spring/SpringBoot/springdatajpa/src/main/java/com/example/springdatajpa/SpringdatajpaApplication.java
+++ Spring/SpringBoot/springdatajpa/src/main/java/com/example/springdatajpa/SpringdatajpaApplication.java
@@ -34,8 +34,13 @@ public class SpringdatajpaApplication implements CommandLineRunner {
 //
 //			entityManager.persist(user); //영속성을 갖게한다 - 프로그램이 종료되어도 남도독한다. -> DMBS에 저장해달라
 
+			//update시에는 entityManager의 메소드를 호출하지 않아도 된다.
+			//Entity Manager는 데이터에 대한 요청을 Persistence Context를 통해 한다.
+			//이떄 요청한 정보에대한 스냅샷을 Persistence Context는 가지고 있다.
+			//commit()이 일어나면 Persistence Context는 스냅샷과 현재의 user를 비교하여 변화가 일어났다면
+			// 자동적으로 DBMS에 변경사항을 저장하는 명령을 내린다.
 			User user = entityManager.find(User.class, 1);
-			System.out.println(user);
+			user.setPassword("5678");
 
 			transaction.commit();
 		} catch (Exception e) {
