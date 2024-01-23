a616b35949bfa2b2b27628571c2f81ee77176939
chobo5 <wnsdus1008@gmail.com>
2024-01-23 21:30:00 +0900

    SQL부터 Spring Data JPA까지 query methods

--- Spring/SpringBoot/springdatajpa/src/main/java/com/example/springdatajpa/SpringdatajpaApplication.java
+++ Spring/SpringBoot/springdatajpa/src/main/java/com/example/springdatajpa/SpringdatajpaApplication.java
@@ -12,27 +12,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpringdatajpaApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringdatajpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdatajpaApplication.class, args);
+    }
 
-	@Autowired
-	UserRepository userRepository; //UserRepository를 구현하는 Bean을 자동으로 inject해준다.
+    @Autowired
+    UserRepository userRepository; //UserRepository를 구현하는 Bean을 자동으로 inject해준다.
 
 
-	@Override
-	@Transactional //메소드가 시작할떄 transaction이 시작, 메서드가 종료될떄 transaction이 commit, 중간에 RuntimeException 발생시 rollback
-	public void run(String... args) throws Exception {
-		//조회
+    @Override
+    @Transactional //메소드가 시작할떄 transaction이 시작, 메서드가 종료될떄 transaction이 commit, 중간에 RuntimeException 발생시 rollback
+    public void run(String... args) throws Exception {
+        //조회
 //		System.out.println(userRepository.getClass().getName());
 //		User user = userRepository.findById(1).orElseThrow();
 //		System.out.println(user);
 
-		//insert
+        //insert
 //		User user = new User();
 //		user.setName("둘리4");
 //		user.setEmail("dool4@test.com");
@@ -45,24 +47,89 @@ public class SpringdatajpaApplication implements CommandLineRunner {
 //			System.out.println("savedUser != user");
 //		}
 
-		//delete
+        //delete
 //		userRepository.deleteById(2);
 
-		//delete2
+        //delete2
 //		User user = userRepository.findById(3).orElseThrow();
 //		userRepository.delete(user);
 
-		//update
+        //update
 //		User user = userRepository.findById(4).orElseThrow();
 //		System.out.println(user);
 //		user.setPassword("5678");
 
-		//이름으로 검색
-		User user = userRepository.findByName("원준연").orElseThrow();
-		System.out.println(user);
+        //이름으로 검색
+//		User user = userRepository.findByName("원준연").orElseThrow();
+//		System.out.println(user);
+//
+//		Optional<User> user2 = userRepository.findByNameAndEmail("둘리", "dool@test.com");
+//		System.out.println(user2.get());
+//
+//		List<User> users = userRepository.findByNameOrEmail("둘리2", "dool@test.com");
+//		for (User userr : users) {
+//			System.out.println(userr);
+//		}
 
+//		List<User> users = userRepository.findByUserIdBetween(1, 3);
+//		for (User userr : users) {
+//			System.out.println(userr);
+//		}
 
+//        List<User> users = userRepository.findByUserIdLessThan(4);
+//        for (User userr : users) {
+//            System.out.println(userr);
+//        }
 
-	}
+//        List<User> users = userRepository.findByUserIdLessThanEqual(4);
+//        for (User userr : users) {
+//            System.out.println(userr);
+//        }
+
+//        List<User> users = userRepository.findByRegdateAfter(LocalDateTime.now().minusDays(2L));
+//        for (User userr : users) {
+//            System.out.println(userr);
+//        }
+
+//        List<User> users = userRepository.findByNameIsNull();
+//        for (User userr : users) {
+//            System.out.println(userr);
+//        }
+
+//        List<User> users = userRepository.findByNameLike("둘리%");
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+
+//        List<User> users = userRepository.findByNameLike("둘리_");
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+
+//        List<User> users = userRepository.findByOrderByNameAsc();
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+
+//        List<User> users = userRepository.findByOrderByNameDesc();
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+
+//        List<User> users = userRepository.findByRegdateBeforeOrderByNameDesc(LocalDateTime.now().minusDays(1L));
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+
+//        List<User> users = userRepository.findByUserIdIn(List.of(2, 3, 5));
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+
+        List<User> users = userRepository.findByUserIdNotIn(List.of(2, 3, 5));
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
 
 }
--- Spring/SpringBoot/springdatajpa/src/main/java/com/example/springdatajpa/repository/UserRepository.java
+++ Spring/SpringBoot/springdatajpa/src/main/java/com/example/springdatajpa/repository/UserRepository.java
@@ -3,11 +3,69 @@ package com.example.springdatajpa.repository;
 import com.example.springdatajpa.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 //Spring Data Jpa Repository 완성
 //보통 interface를 선언하면 interface를 구현하는 클래스를 작성한다.
 //하지만 Spring Data Jpa는 UserRepository를 구현하는 bean을 자동으로 생성한다.
 public interface UserRepository extends JpaRepository<User, Integer> {
+    // where name = ?
     Optional<User> findByName(String name); //query method - Spring Data JPA
+    //where name = ? and email = ?
+    Optional<User> findByNameAndEmail(String name, String email);
+    //where name = ? or email = ?
+    List<User> findByNameOrEmail(String name, String email);
+    //where user_id between ? and ?
+    List<User> findByUserIdBetween(int startUserId, int endUserId);
+    //where user_id < ?
+    List<User> findByUserIdLessThan(int userId);
+    //where user_id <= ?
+    List<User> findByUserIdLessThanEqual(int userId);
+    //where user_id > ?
+    List<User> findByUserIdGreaterThan(int userId);
+    //where user_id >= ?
+    List<User> findByUserIdGreaterThanEqual(int userId);
+
+    //where regdate > ?
+    List<User> findByRegdateAfter(LocalDateTime day);
+
+    //where regdate < ?
+    List<User> findByRegdateBefore(LocalDateTime day);
+
+    //where name is null
+    List<User> findByNameIsNull();
+
+    //where name is not null;
+    List<User> findByNameIsNotNull();
+
+    //where name like ? (직접 % or _ 입력)
+    List<User> findByNameLike(String name);
+
+    //where name like ?%
+    List<User> findByNameStartingWith(String name);
+
+    //where name like %?
+    List<User> findByNameEndingWith(String name);
+
+    //where name like %?%
+    List<User> findByNameContaining(String name);
+
+    //select * from user3 order by name asc;
+    List<User> findByOrderByNameAsc();
+
+    //select * from user3 order by name desc;
+    List<User> findByOrderByNameDesc();
+
+    List<User> findByRegdateBeforeOrderByNameDesc(LocalDateTime day);
+
+    //where name <> ? (null 비교 불가)
+    List<User> findByNameNot(String name);
+
+    //where user_id in (...);
+    List<User> findByUserIdIn(Collection<Integer> userIds);
+
+    List<User> findByUserIdNotIn(Collection<Integer> userIds);
 }
