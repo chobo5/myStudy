f7c2e073241824b38afcd819c6f287acbcf1240c
chobo5 <wnsdus1008@gmail.com>
2024-01-23 19:28:53 +0900

    multiThread - 01

--- This_is_Java/src/multiThread/ThreadExam.java
+++ This_is_Java/src/multiThread/ThreadExam.java
@@ -1,2 +1,22 @@
-package multiThread;public class ThreadExam {
+package multiThread;
+
+public class ThreadExam {
+    public static void main(String[] args) {
+        //방법1
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        });
+        t.start();
+
+        //방법2
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName());
+        }).start();
+    }
+
+
+
 }
