64a13e15049dca3ca52be2a8e8f80618893e48ec
chobo5 <wnsdus1008@gmail.com>
2024-01-25 13:29:02 +0900

    muti-thread thread pool 구현해보고 적용하기

---
+++ bitcamp/myapp/app-server/src/main/java/bitcamp/util/ThreadPool.java
@@ -0,0 +1,22 @@
+package bitcamp.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadPool implements Pooling<WorkerThread>{
+    List<WorkerThread> list = new ArrayList<>();
+    @Override
+    public WorkerThread get() {
+        if (list.size() > 0) {
+            return list.remove(0);
+        }
+        WorkerThread thread = new WorkerThread(this);
+        thread.start();
+        return thread;
+    }
+
+    @Override
+    public void revert(WorkerThread thread) {
+        list.add(thread);
+    }
+}
