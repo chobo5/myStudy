b5091bd2cccde6b20600ebc184d9de4fe5a71d65
chobo5 <wnsdus1008@gmail.com>
2024-01-29 16:51:33 +0900

    programmers 약수의 합

--- Algorithm/Programmers/src/_약수의합.java
+++ Algorithm/Programmers/src/_약수의합.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class _약수의합 {
+public class _약수의합 {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int n) {
+                int answer = 0;
+                for (int i = 1; i <= n ; i++) {
+                    if (n % i == 0) {
+                        answer += i;
+                    }
+                }
+
+                return answer;
+            }
+        }
+    }
 }
