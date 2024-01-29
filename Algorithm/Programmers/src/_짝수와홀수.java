f103d980ab0ad2403349eb2e7da370d4b191cb15
chobo5 <wnsdus1008@gmail.com>
2024-01-29 14:59:29 +0900

    programmers 짝수와 홀수

---
+++ Algorithm/Programmers/src/_짝수와홀수.java
@@ -0,0 +1,11 @@
+public class _짝수와홀수 {
+    class Solution {
+        public String solution(int num) {
+            if (num % 2 == 1) {
+                return "Odd";
+            } else {
+                return "Even";
+            }
+        }
+    }
+}
