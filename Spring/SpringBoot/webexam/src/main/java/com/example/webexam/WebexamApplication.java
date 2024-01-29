28764d91fa086c424a74041c18b2218ef1b5fe8c
chobo5 <wnsdus1008@gmail.com>
2024-01-29 21:30:25 +0900

    23 - SpringBoot & Spring MVC

--- Spring/SpringBoot/webexam/src/main/java/com/example/webexam/WebexamApplication.java
+++ Spring/SpringBoot/webexam/src/main/java/com/example/webexam/WebexamApplication.java
@@ -1,13 +1,28 @@
 package com.example.webexam;
 
+import ch.qos.logback.classic.pattern.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.List;
 
 @SpringBootApplication
 public class WebexamApplication {
 
+	@Autowired
+	List<HttpMessageConverter> messageConverters;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebexamApplication.class, args);
+
+	}
+
+	public void run(String... args) throws Exception {
+		for (HttpMessageConverter messageConverter : messageConverters) {
+			System.out.println(messageConverter.getClass().getName());
+		}
 	}
 
 }
--- Spring/SpringBoot/webexam/src/main/java/com/example/webexam/controller/ExampleController.java
+++ Spring/SpringBoot/webexam/src/main/java/com/example/webexam/controller/ExampleController.java
@@ -21,8 +21,8 @@ public class ExampleController {
     }
 
     @PostMapping("/3")
-    public String example3(@RequestParam(name = "name") String name,
-                           @RequestParam(name = "email") String email) {
+    public String example3(@RequestParam(name = "name", required = true) String name,
+                           @RequestParam(name = "email", required = false, defaultValue = "empty_email") String email) {
         //form으로 부턴 넘어 값을 받는방법
         //방법 1. HttpServletRequest 이용
 //        System.out.println(request.getParameter("name"));
@@ -38,9 +38,22 @@ public class ExampleController {
     //@RestController는 보통 브라우저에서 동작하는 JavaScript가 호출한다.
     //JavaScript가 호출할때 json데이터를 post방식으로 body에 담아 전송한다
     @PostMapping("/4")
-    public String example4(@RequestBody User user) {
+    public User example4(@RequestBody User user) {
         System.out.println(user);
         user.setRandom(new Random().nextInt(100));
-        return "example4";
+        return user;
+    }
+
+    @GetMapping("pathexam/{id}")
+    public String pathExam(@PathVariable(name = "id") String id) {
+        System.out.println(id);
+        return "pathexam";
+    }
+
+    @GetMapping("/headerexam")
+    public String headerExam(@RequestHeader("User-Agent") String userAgentValue) {
+        System.out.println(userAgentValue);
+        return "headerexam";
+
     }
 }
