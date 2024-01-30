9a0090415aa88c5bdb326e7418ba0e27371cf21e
chobo5 <wnsdus1008@gmail.com>
2024-01-30 13:58:55 +0900

    unsolved

---
+++ Algorithm/Programmers/src/unsolved/_JumpandTeleport.java
@@ -0,0 +1,18 @@
+package unsolved;
+
+public class _JumpandTeleport {
+    public class Solution {
+        public int solution(int n) {
+            int ans = 0;
+            int num = n;
+            for (int i = 1; i <= n; i++) {
+                while (num > 0) {
+
+                }
+
+            }
+
+            return ans;
+        }
+    }
+}
