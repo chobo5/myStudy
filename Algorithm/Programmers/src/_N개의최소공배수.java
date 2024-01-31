09e4760e7007c97d66b7095a7cf1606ccb98fe0c
chobo5 <wnsdus1008@gmail.com>
2024-01-31 20:29:49 +0900

    programmers n개의 최소공배수

--- Algorithm/Programmers/src/_N개의최소공배수.java
+++ Algorithm/Programmers/src/_N개의최소공배수.java
@@ -2,21 +2,28 @@ public class _N개의최소공배수 {
     public static void main(String[] args) {
         class Solution {
             public int solution(int[] arr) {
-                int answer = 1;
+                int answer = arr[0];
+                if (arr.length == 1) {
+                    return arr[0];
+                }
+
                 for (int i = 0; i < arr.length - 1; i++) {
-                    int min = gcd(arr[i], arr[i + 1]);
-                    answer *= min;
+                    answer = lcm(answer, arr[i + 1]);
                 }
                 return answer;
             }
 
             int gcd(int a, int b) {
                 int r = a % b;
-                if(r == 0) {
+                if (r == 0) {
                     return b;
                 } else {
                     return gcd(b, r);
                 }
+            }
+
+            int lcm(int a, int b) {
+                return a * b /gcd(a, b);
             }
         }
         int[] test = {1, 2, 3};
