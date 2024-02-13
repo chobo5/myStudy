1f13270bd475d19d5bbe62b8030dca164d588ea6
chobo5 <wnsdus1008@gmail.com>
2024-02-13 18:55:47 +0900

    programmers 숫자짝궁

--- Algorithm/Programmers/src/_숫자짝궁.java
+++ Algorithm/Programmers/src/_숫자짝궁.java
@@ -1,24 +1,34 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class _숫자짝궁 {
     public static void main(String[] args) {
         class Solution {
             public String solution(String X, String Y) {
                 StringBuilder sb = new StringBuilder();
-                List<String> common = new ArrayList<>();
-                String[] arrY = Y.split("");
+                int[] x = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                int[] y = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
                 for (int i = 0; i < X.length(); i++) {
-                    if (Arrays.asList(arrY).contains(X.charAt(i))) {
-                        common.add(String.valueOf(X.charAt(i)));
+                    x[X.charAt(i) - 48] += 1;
+                }
+                for (int i = 0; i < Y.length(); i++) {
+                    y[Y.charAt(i) - 48] += 1;
+                }
+                for (int i = 9; i >= 0; i--) {
+                    for (int j = 0; j < Math.min(x[i], y[i]); j++) {
+                        sb.append(i);
                     }
                 }
-                 common.stream().sorted().forEach(sb::append);
-                String answer = sb.toString().length() == 0 ? "-1" : sb.toString();
-                if (Integer.parseInt(answer) == 0) return "0";
-                return answer;
+                if("".equals(sb.toString())){
+                    return "-1";
+                }else if(sb.toString().charAt(0)==48){
+                    return "0";
+                }else {
+                    return sb.toString();
+                }
+
             }
         }
+
+        System.out.println(new Solution().solution("5525", "1255"));
     }
 }
