12b054d1609af9bf3c3e3b9199d6341123b71745
chobo5 <wnsdus1008@gmail.com>
2024-02-15 18:54:43 +0900

    로그인/로그아웃 구현, 게시물 CUD시 로그인정보 사용, 웹 어플리케이 서버 구조로 전환(TomcatServer 사용), Servlet

---
+++ bitcamp/myapp/app/src/main/java/bitcamp/myapp/handler/board/BoardAddHandler.java
@@ -0,0 +1,70 @@
+package bitcamp.myapp.handler.board;
+
+import bitcamp.menu.AbstractMenuHandler;
+import bitcamp.myapp.dao.AttachedFileDao;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.vo.AttachedFile;
+import bitcamp.myapp.vo.Board;
+import bitcamp.myapp.vo.Member;
+import bitcamp.util.Prompt;
+import bitcamp.util.DBConnectionPool;
+import bitcamp.util.Session;
+import bitcamp.util.TransactionManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class BoardAddHandler extends AbstractMenuHandler {
+
+    TransactionManager txManager;
+    private BoardDao boardDao;
+    private AttachedFileDao attachedFileDao;
+
+    public BoardAddHandler(TransactionManager txManager, BoardDao boardDao, AttachedFileDao attachedFileDao) {
+        this.txManager = txManager;
+        this.boardDao = boardDao;
+        this.attachedFileDao = attachedFileDao;
+    }
+
+    @Override
+    protected void action(Prompt prompt) {
+        Member loginUser = (Member) prompt.getSession().getAttribute("loginUser");
+        if (loginUser == null) {
+            prompt.println("로그인이 필요합니다");
+            return;
+        }
+        Board board = new Board();
+        board.setTitle(prompt.input("제목? "));
+        board.setContent(prompt.input("내용? "));
+        board.setWriter(loginUser);
+
+        ArrayList<AttachedFile> files = new ArrayList<>();
+        while (true) {
+            String filePath = prompt.input("파일?(종료: 엔터) ");
+            if (filePath.length() == 0) {
+                break;
+            }
+            files.add(new AttachedFile().filePath(filePath));
+        }
+
+        try {
+            txManager.startTransaction();
+            boardDao.add(board);
+            if (files.size() > 0) {
+                //첨부 파일 객체에 게시글 번호 저장
+                for (AttachedFile file : files) {
+                    file.setBoardNo(board.getNo());
+                }
+                attachedFileDao.addAll(files);
+            }
+            txManager.commit();
+        } catch (Exception e) {
+            try {
+                txManager.rollback();
+                System.out.println("게시글 등록 오류");
+            } catch (Exception ex) {
+            }
+        }
+    }
+}
---
+++ bitcamp/myapp/app/src/main/java/bitcamp/myapp/servlet/board/BoardListServlet.java
@@ -0,0 +1,71 @@
+package bitcamp.myapp.servlet.board;
+
+import bitcamp.menu.AbstractMenuHandler;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.dao.mysql.BoardDaoImpl;
+import bitcamp.myapp.vo.Board;
+import bitcamp.util.DBConnectionPool;
+import bitcamp.util.Prompt;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/board/list")
+public class BoardListServlet extends GenericServlet {
+    private BoardDao boardDao;
+
+    public BoardListServlet() {
+        DBConnectionPool connectionPool = new DBConnectionPool(
+                "jdbc:mysql://db-ld296-kr.vpc-pub-cdb.ntruss.com/studydb", "study", "Bitcamp!@#123");
+        this.boardDao = new BoardDaoImpl(connectionPool, 1);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = res.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang='en'>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<title>Hello World</title>");
+        out.println("</head>");
+        out.println("<body>");
+        try {
+            out.println("<table>");
+            out.println("<thead>");
+            out.println("<tr>");
+            out.println("<th>번호</th> <th>제목</th> <th>작성자</th> <th>등록일</th> <th>첨부 파일</th>");
+            out.println("</tr>");
+            out.println("</thead>");
+            out.println("<tbody>");
+            List<Board> list = boardDao.findAll();
+
+            for (Board board : list) {
+                out.printf("<tr> <td>%d</td> <td>%s</td> <td>%s</td> <td>%s</td> <td>%d</td> </tr>\n",
+                        board.getNo(),
+                        board.getTitle(),
+                        board.getWriter().getName(),
+                        board.getCreatedDate(),
+                        board.getFileCount());
+            }
+            out.println("</tbody>");
+            out.println("</table>");
+        } catch (Exception e) {
+            out.println("<p>목록 오류</p>");
+            out.println("<pre>");
+            e.printStackTrace(out);
+            out.println("</pre>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+
+}
