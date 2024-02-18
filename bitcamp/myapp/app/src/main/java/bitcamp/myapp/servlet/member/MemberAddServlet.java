f583f6d43253c7e8312afdd752342ed0139e346e
chobo5 <wnsdus1008@gmail.com>
2024-02-18 18:14:42 +0900

    myapp crud 완료

--- bitcamp/myapp/app/src/main/java/bitcamp/myapp/servlet/member/MemberAddServlet.java
+++ bitcamp/myapp/app/src/main/java/bitcamp/myapp/servlet/member/MemberAddServlet.java
@@ -1,4 +1,72 @@
 package bitcamp.myapp.servlet.member;
 
-public class MemberAddServlet {
+import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.dao.mysql.MemberDaoImpl;
+import bitcamp.myapp.vo.AttachedFile;
+import bitcamp.myapp.vo.Board;
+import bitcamp.myapp.vo.Member;
+import bitcamp.util.DBConnectionPool;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/member/add")
+public class MemberAddServlet extends HttpServlet {
+    MemberDao memberDao;
+    public MemberAddServlet() {
+        DBConnectionPool connectionPool = new DBConnectionPool(
+                "jdbc:mysql://db-ld296-kr.vpc-pub-cdb.ntruss.com/studydb", "study", "Bitcamp!@#123");
+        memberDao = new MemberDaoImpl(connectionPool);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang='en'>");
+        out.println("<head>");
+        out.println("  <meta charset='UTF-8'>");
+        out.println("  <title>비트캠프 데브옵스 5기</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>회원 가입</h1>");
+
+        try {
+            String email = req.getParameter("email");
+            String name = req.getParameter("name");
+            String password = req.getParameter("password");
+            String password2 = req.getParameter("password2");
+            if (!password.equals(password2)) {
+                out.println("<p>비밀번호가 일치하지 않습니다.</p>");
+                out.println("<pre>");
+                out.println("</pre>");
+                return;
+            }
+            Member member = new Member();
+            member.setEmail(email);
+            member.setName(name);
+            member.setPassword(password);
+            memberDao.add(member);
+            out.println("<p>회원가입 완료!</p>");
+            out.println("<pre>");
+            out.println("</pre>");
+
+        } catch (Exception e) {
+            out.println("<p>조회 오류!</p>");
+            out.println("<pre>");
+            e.printStackTrace(out);
+            out.println("</pre>");
+        }
+
+        out.println("</body>");
+        out.println("</html>");
+    }
 }
