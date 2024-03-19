2916ea5900382bf5d9bfd2bfd6595c790f996cc0
chobo5 <wnsdus1008@gmail.com>
2024-03-19 20:32:57 +0900

    .

--- bitcamp/mvc-app/app/src/main/java/bitcamp/config/App2WebApplicationInitializer.java
+++ bitcamp/mvc-app/app/src/main/java/bitcamp/config/App2WebApplicationInitializer.java
@@ -4,6 +4,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+import java.io.File;
+
 public class App2WebApplicationInitializer extends
     AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -27,5 +31,15 @@ public class App2WebApplicationInitializer extends
   @Override
   protected String getServletName() {
     return "app2";
+  }
+
+  @Override
+  protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+    registration.setMultipartConfig(new MultipartConfigElement(
+            new File("./temp").getAbsolutePath(),
+            //new File(System.getProperty("java.io.tmpdir")).getAbsolutePath(),
+            1024 * 1024 * 10,
+            1024 * 1024 * 100,
+            1024 * 1024 * 1));
   }
 }
