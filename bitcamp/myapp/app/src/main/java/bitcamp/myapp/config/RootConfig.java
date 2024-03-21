614303b3d6baaa53af2dd696dec2a1ae084cfabc
chobo5 <wnsdus1008@gmail.com>
2024-03-21 23:34:14 +0900

    mybatis 적용

--- bitcamp/myapp/app/src/main/java/bitcamp/myapp/config/RootConfig.java
+++ bitcamp/myapp/app/src/main/java/bitcamp/myapp/config/RootConfig.java
@@ -1,9 +1,15 @@
 package bitcamp.myapp.config;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+
+import java.io.InputStream;
 
 @ComponentScan(value={"bitcamp.myapp.dao", "bitcamp.util"})
 @PropertySource({"classpath:config/jdbc.properties"})
@@ -11,5 +17,14 @@ public class RootConfig {
     private final Log log = LogFactory.getLog(RootConfig.class);
     public RootConfig() {
         log.debug("생성자 호출");
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        String resource = "config/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+        return sqlSessionFactory;
     }
 }
