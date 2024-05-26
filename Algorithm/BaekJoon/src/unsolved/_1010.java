7aaa625131ee390c385bae4d89a0cd4e888bdb67
JUNYEON WON <wnsdus1008@gmail.com>
2024-05-26 22:15:27 +0900

    baekjoon 1010

--- Algorithm/BaekJoon/src/unsolved/_1010.java
+++ Algorithm/BaekJoon/src/unsolved/_1010.java
@@ -6,30 +6,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _1010 {
+
+    static int[][] all = new int[30][30];
+
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(in.readLine());
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < t; i++) {
-            String str = in.readLine();
-            st = new StringTokenizer(str, " ");
+            st = new StringTokenizer(in.readLine(), " ");
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-
-            long result = factorial(m) / (factorial(n) * factorial(m - n));
-            sb.append(result).append("\n");
+            sb.append(combination(m, n)).append("\n");
         }
-        in.close();
         System.out.println(sb);
-
     }
 
-    static long factorial(int num) {
-        long result = 1;
-        for (int i = 1; i <= num; i++) {
-            result *= i;
+    static int combination(int a, int b) {
+        if (all[a][b] > 0) {
+            return all[a][b];
         }
-        return result;
+        if (a == b || b == 0) {
+            return all[a][b] = 1;
+        }
+        return combination(a - 1, b - 1) + combination(a - 1, b);
+
     }
 }
