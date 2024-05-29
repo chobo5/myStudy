1fb12e910830b6baecab767bd7f58f3732931b8e
JUNYEON WON <wnsdus1008@gmail.com>
2024-05-29 17:13:35 +0900

    baekjoon 24263

---
+++ Algorithm/BaekJoon/src/_24263.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
