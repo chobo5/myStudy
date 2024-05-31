29642c501b5125dc47d2a0fd9c2434957eba44e6
JUNYEON WON <wnsdus1008@gmail.com>
2024-05-31 18:46:25 +0900

    baekjoon 1436

--- Algorithm/BaekJoon/src/_1436.java
+++ Algorithm/BaekJoon/src/_1436.java
@@ -8,16 +8,42 @@ public class _1436 {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(in.readLine());
         int current = 666;
-        for (int i = 0; i < n; i++) {
-            int next1 = current + 1000;
-            int next2 = current * 10;
-            if (next1 > current) {
-
+        int count =  1;
+        while (count != n) {
+            current++;
+            if (String.valueOf(current).contains("666")) {
+                count++;
             }
         }
+
+        System.out.println(current);
+
     }
 
 
 }
 
 
+/*
+1 666
+2 1666
+3 2666
+4 3666
+5 4666
+6 5666
+7 6666
+8 6660
+9 6661
+10 6662
+11 6663
+ .
+ .
+17 6669
+18 7666
+19 8666
+20 9666
+21 10666
+22 11666
+27 16660
+
+ */
