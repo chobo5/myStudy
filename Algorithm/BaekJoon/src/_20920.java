ac93924ee085b5a39392e6a6590d8e2a79ac6bb4
JUNYEON WON <wnsdus1008@gmail.com>
2024-06-03 15:42:12 +0900

    baekjoon 20920

--- Algorithm/BaekJoon/src/_20920.java
+++ Algorithm/BaekJoon/src/_20920.java
@@ -6,27 +6,45 @@ import java.util.*;
 public class _20920 {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String str = in.readLine();
-        StringTokenizer st = new StringTokenizer(str, " ");
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        Map<String, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < n ; i++) {
+        int m = Integer.parseInt(st.nextToken());
+        Map<String, Integer> wordAndCount = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
             String word = in.readLine();
-            if (word.length() >= 4) {
-                if (hashMap.containsKey(word)) {
-                    int num = hashMap.get(word);
-                    hashMap.replace(word, num + 1);
-                } else {
-                    hashMap.put(word, 1);
-                }
+            if (word.length() < m) {
+                continue;
             }
-        }
-//        Collections.sort();
-        Iterator iterator = hashMap.keySet().iterator();
-        while (iterator.hasNext()) {
-           String key = (String) iterator.next();
+            int count = wordAndCount.get(word) == null ? 1 : wordAndCount.get(word) + 1;
+
+            wordAndCount.put(word, count);
         }
 
+        List<Map.Entry<String, Integer>> words = new ArrayList<>(wordAndCount.entrySet());
+
+        words.sort(((o1, o2) -> {
+            if (o1.getValue() != o2.getValue()) {
+                return o2.getValue() - o1.getValue();
+            }
+
+            if (o1.getKey().length() != o2.getKey().length()) {
+                return o2.getKey().length() - o1.getKey().length();
+            }
+
+            return o1.getKey().compareTo(o2.getKey());
+
+        }));
+
+        StringBuilder sb = new StringBuilder();
+        words.forEach(e -> sb.append(e.getKey()).append("\n"));
+
+        System.out.println(sb);
+
+
+
+
     }
+
+
 }
