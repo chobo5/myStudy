82d38561d61943f339e6ed612acbf16a5f4fea70
JUNYEON WON <wnsdus1008@gmail.com>
2024-06-04 17:17:10 +0900

    programmers 2016년

--- Algorithm/Programmers/src/_2016년.java
+++ Algorithm/Programmers/src/_2016년.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class _2016년 {
+public class _2016년 {
+    public static void main(String[] args) {
+        class Solution {
+            public String solution(int a, int b) {
+
+                int days = 0;
+                for (int i = 1; i < a; i++) {
+                    if (i == 2) {
+                        days += 29;
+                        continue;
+                    }
+
+
+                    if (i % 2 == 0) {
+                        if (i >= 8) {
+                            days += 31;
+                        } else {
+                            days += 30;
+                        }
+                    } else {
+                        if (i <= 7) {
+                            days += 31;
+                        } else {
+                            days += 30;
+                        }
+
+                    }
+                }
+
+                int day = (days + b - 1) % 7;
+                String[] names = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+                return names[day];
+            }
+        }
+
+    }
 }
