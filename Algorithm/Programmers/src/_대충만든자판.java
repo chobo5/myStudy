bd7d7e3e4e46ba4f7f28788c5832b3b481d4077e
JUNYEON WON <wnsdus1008@gmail.com>
2024-06-05 13:29:09 +0900

    programmers 대충만든자판

--- Algorithm/Programmers/src/_대충만든자판.java
+++ Algorithm/Programmers/src/_대충만든자판.java
@@ -2,35 +2,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class _대충만든자판 {
-    class Solution {
-        public int[] solution(String[] keymap, String[] targets) {
-            int[] answer = new int[targets.length];
-            Map<Character, Integer> map = new HashMap<>();
-            for (int i = 0; i < keymap.length; i++) {
-                for (int j = 0; j < keymap[i].length(); j++) {
-                    if (map.containsKey(keymap[i].charAt(j))) {
-                        if (map.get(keymap[i].charAt(j)) > j + 1) {
-                            map.put(keymap[i].charAt(j), j + 1);
+    public static void main(String[] args) {
+        class Solution {
+            public static int[] solution(String[] keymap, String[] targets) {
+                int[] answer = new int[targets.length];
+                Map<String, Integer> map = new HashMap<>();
+
+                for (int i = 0; i < keymap.length; i++) {
+                    for (int j = 0; j < keymap[i].length(); j++) {
+                        String s = String.valueOf(keymap[i].charAt(j));
+                        if (map.get(s) != null) {
+                            map.put(s, Math.min(j + 1, map.get(s)));
+                            continue;
                         }
-                    } else {
-                        map.put(keymap[i].charAt(j), j + 1);
+                        map.put(s, j + 1);
                     }
                 }
-            }
-            int count = 0;
-            for (int i = 0; i < targets.length; i++) {
-                for (int j = 0; j < targets[i].length(); j++) {
-                    if (!map.containsKey(targets[i].charAt(j))) {
-                        int[] error = {-1};
-                        return error;
-                    } else {
-                        count += map.get(targets[i].charAt(j));
+
+                for (int i = 0; i < targets.length; i++) {
+                    int result = 0;
+                    for (int j = 0; j < targets[i].length(); j++) {
+                        String s = String.valueOf(targets[i].charAt(j));
+                        int temp = map.get(s) == null ? -1 : map.get(s);
+                        if (temp == -1) {
+                            result = -1;
+                            break;
+                        }
+                        result += temp;
                     }
+                    answer[i] = result;
                 }
-                answer[i] = count;
-                count = 0;
+                return answer;
             }
-            return answer;
         }
+        String[] keymap = {"ABACD", "BCEFD"};
+        String[] target = {"ABCD", "AABB"};
+        System.out.println(Solution.solution(keymap, target));
     }
 }
