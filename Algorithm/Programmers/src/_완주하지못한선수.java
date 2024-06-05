e3e49ad1db6b87f94faa8488a6cb18cb66faa25b
JUNYEON WON <wnsdus1008@gmail.com>
2024-06-05 13:47:35 +0900

    programmers 완주하지못한선수

--- Algorithm/Programmers/src/_완주하지못한선수.java
+++ Algorithm/Programmers/src/_완주하지못한선수.java
@@ -1,28 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class _완주하지못한선수 {
     public static void main(String[] args) {
         class Solution {
             public String solution(String[] participant, String[] completion) {
                 String answer = "";
+                Map<String, Integer> hashMap = new HashMap<>();
                 for (int i = 0; i < participant.length; i++) {
                     String p = participant[i];
-                    boolean isChanged = false;
-                    for (int j = 0; j < completion.length; j++) {
-                        if (p.equals(completion[j])) {
-                            completion[j] = completion[j].replace(completion[j], "");
-                            isChanged = true;
-                            break;
-                        }
+                    if (hashMap.get(p) == null) {
+                        hashMap.put(p, 1);
+                        continue;
                     }
-                    if (isChanged == false) {
-                        answer = participant[i];
-                        break;
-                    }
+                    hashMap.put(p, hashMap.get(p) + 1);
                 }
-                return answer;
+
+                for (int i = 0; i < completion.length; i++) {
+                    String c = completion[i];
+                    if (hashMap.get(c) == null) {
+                        return c;
+                    }
+
+                    if (hashMap.get(c) > 1) {
+                        hashMap.put(c, hashMap.get(c) - 1);
+                        continue;
+                    }
+                    hashMap.remove(c);
+                }
+
+                return (String) hashMap.keySet().toArray()[0];
             }
         }
-        String[] p = {"mislav", "stanko", "mislav", "ana"};
-        String[] c = {"stanko", "ana", "mislav"};
-        System.out.println(new Solution().solution(p, c));
     }
 }
