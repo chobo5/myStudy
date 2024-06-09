a70579d3c9380a6b31804e9b8c0548bea7b507c1
JUNYEON WON <wnsdus1008@gmail.com>
2024-06-09 15:49:03 +0900

    1차 캐시

--- Algorithm/Programmers/src/_1차캐시.java
+++ Algorithm/Programmers/src/_1차캐시.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class _1차캐시 {
+import java.util.ArrayList;
+import java.util.List;
+
+public class _1차캐시 {
+    class Solution {
+        public int solution(int cacheSize, String[] cities) {
+            int answer = 0;
+
+            if (cacheSize == 0) {
+                return cities.length * 5;
+            }
+
+            List<String> cache = new ArrayList<>();
+            for (String city : cities) {
+                String cityLower = city.toLowerCase();
+                if (cache.contains(cityLower)) {
+                    cache.remove(cityLower);
+                    cache.add(cityLower);
+                    answer += 1;
+                } else {
+                    if (cache.size() == cacheSize) {
+                        cache.remove(0);
+                    }
+                    cache.add(cityLower);
+                    answer += 5;
+                }
+            }
+            return answer;
+        }
+    }
 }
