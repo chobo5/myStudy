702d5e9c56a17ddf2992ba482b24a1ebf4853d2d
JUNYEON WON <wnsdus1008@gmail.com>
2024-06-12 12:50:35 +0900

    programmers 다음에 올 숫자

---
+++ Algorithm/Programmers/src/_다음에올숫자.java
@@ -0,0 +1,17 @@
+public class _다음에올숫자 {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int[] common) {
+                int answer = 0;
+
+                if (common[1] - common[0] == common[2] - common[1]) {
+                    answer = common[common.length - 1] + (common[1] - common[0]);
+                } else {
+                    answer = common[common.length - 1]  * (common[1] / common[0]);
+                }
+
+                return answer;
+            }
+        }
+    }
+}
