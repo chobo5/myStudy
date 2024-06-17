c9bf863d844e80deddddce2473e2454f15c26b4f
JUNYEON WON <wnsdus1008@gmail.com>
2024-06-17 17:30:35 +0900

    programmers 정사각형으로만들기

--- Algorithm/Programmers/src/_정사각형으로만들기.java
+++ Algorithm/Programmers/src/_정사각형으로만들기.java
@@ -1,2 +1,26 @@
-public class _정사각형만들기 {
+public class _정사각형으로만들기 {
+    class Solution {
+        public int[][] solution(int[][] arr) {
+            int a = arr[0].length;
+            int b = arr.length;
+            
+            if (a > b) {
+                int[][] answer = new int[a][a];
+                for (int i = 0; i < b; i++) {
+                    for (int j = 0; j < a; j++) {
+                        answer[i][j] = arr[i][j];
+                    }
+                }
+                return answer;
+            } else {
+                int[][] answer = new int[b][b];
+                for (int i = 0; i < b; i++) {
+                    for (int j = 0; j < a; j++) {
+                        answer[i][j] = arr[i][j];
+                    }
+                }
+               return answer;
+            }
+        }
+    }
 }
