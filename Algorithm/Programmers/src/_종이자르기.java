708991f28808e5ec8b4ac63f3875612e24f6a4a6
JUNYEON WON <wnsdus1008@gmail.com>
2024-06-18 15:34:18 +0900

    programmers 종이자르기

---
+++ Algorithm/Programmers/src/_종이자르기.java
@@ -0,0 +1,12 @@
+public class _종이자르기 {
+    class Solution {
+        public int solution(int M, int N) {
+            int answer = M - 1 + M * (N - 1);
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
