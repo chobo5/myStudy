95daa7d5d57afc941e68c28edb01478a111ddbb1
JUNYEON WON <wnsdus1008@gmail.com>
2024-06-25 13:59:55 +0900

    baekjoon 15651

--- Algorithm/BaekJoon/src/_15651.java
+++ Algorithm/BaekJoon/src/_15651.java
@@ -1,13 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class _15651 {
+
+    static int n;
+    static int m;
+    static int[] arr;
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+        in.close();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        sb = new StringBuilder();
+        dfs(0);
+        System.out.println(sb);
 
+    }
 
+    public static void dfs(int depth) {
+        if (m == depth) {
+            for(int x : arr) {
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+        }
     }
 }
