188c38d1e6376fee5c6a2e9468e6df59a5d91240
JUNYEON WON <wnsdus1008@gmail.com>
2024-07-01 15:25:12 +0900

    programmers 1차뉴스클러스터링

--- Algorithm/Programmers/src/_1차뉴스클러스터링.java
+++ Algorithm/Programmers/src/_1차뉴스클러스터링.java
@@ -1,9 +1,7 @@
-package unsolved;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class _뉴스클러스터링 {
+public class _1차뉴스클러스터링 {
     public static void main(String[] args) {
         class Solution {
             public static int solution(String str1, String str2) {
@@ -14,23 +12,22 @@ public class _뉴스클러스터링 {
                 str2List = getStrList(str2, str2List);
 
                 int same = 0;
+                int sum = str1List.size() + str2List.size();
 
                 if (str1List.size() == 0 && str2List.size() == 0) {
                     return 65536;
                 }
-
-                for (int i = 0; i < str1List.size(); i++) {
-                    String e = str1List.get(i);
-                    for (int j = 0; j < str2List.size(); j++) {
-                        if (e.equals(str2List.get(j))) {
-                            same++;
-                            break;
-                        }
+                List<String> longerList = str1List.size() > str2List.size() ? str1List : str2List;
+                List<String> shorterList = str1List.size() <= str2List.size() ? str1List : str2List;
+                for (int i = 0; i < shorterList.size(); i++) {
+                    String e = shorterList.get(i);
+                    if (longerList.contains(e)) {
+                        longerList.remove(e);
+                        same++;
                     }
                 }
 
-
-                double j = (double) same / (str1List.size() + str2List.size() - same);
+                double j = (double) same / (sum - same);
 
                 return (int) Math.floor(j * 65536);
             }
@@ -40,20 +37,18 @@ public class _뉴스클러스터링 {
                     char first = Character.toUpperCase(str.charAt(i));
                     char second = Character.toUpperCase(str.charAt(i + 1));
 
-                    if (!validate(first) || !validate(second)) {
-                        continue;
+                    if (validate(first) && validate(second)) {
+                        list.add(String.valueOf(first) + String.valueOf(second));
                     }
-
-                    list.add(String.valueOf(first) + String.valueOf(second));
                 }
                 return  list;
             }
 
             public static boolean validate(char c) {
-                if (c > 90 || c < 65) {
-                    return false;
+                if (c <= 90 && c >= 65) {
+                    return true;
                 }
-                return true;
+                return false;
             }
         }
 
