2fdd716ddd419152060e6c1ed09256b80f06c94e
JUNYEON WON <wnsdus1008@gmail.com>
2024-07-16 20:48:44 +0900

    baekjoon 15666

---
+++ Algorithm/BaekJoon/src/_15666.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _15666 {
+    static int n;
+    static int m;
+
+    static int[] arr;
+    static int[] temp;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        temp = new int[m];
+        sb = new StringBuilder();
+        st = new StringTokenizer(in.readLine(), " ");
+        in.close();
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0, 0);
+        System.out.println(sb);
+
+    }
+
+    static void dfs(int depth, int start) {
+        if (depth == m) {
+            for (int i = 0; i < temp.length; i++) {
+                sb.append(temp[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = 0;
+        for (int i = start; i < n; i++) {
+            int now = arr[i];
+            if (before != now) {
+                before = now;
+                temp[depth] = now;
+                dfs(depth + 1, i);
+            }
+        }
+    }
+}
