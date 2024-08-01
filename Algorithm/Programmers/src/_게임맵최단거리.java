da382c8720b048bb963ff0f152dd5dd66ba21cf1
JUNYEON WON <wnsdus1008@gmail.com>
2024-08-01 17:07:12 +0900

    programmers 모음사전

---
+++ Algorithm/Programmers/src/_게임맵최단거리.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class _게임맵최단거리 {
+}
--- Algorithm/Programmers/src/_모음사전.java
+++ Algorithm/Programmers/src/_모음사전.java
@@ -1,2 +1,39 @@
-public class _모음단어 {
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _모음사전 {
+    class Solution {
+        static int index;
+        static int answer;
+        static char[] dictionary;
+        static Map<String, Integer> map;
+        public static int solution(String word) {
+
+            dictionary = new char[]{'A', 'E', 'I', 'O', 'U'};
+            index = 1;
+            answer = 0;
+            map = new HashMap<>();
+            for (int i = 0; i < dictionary.length; i++) {
+                dfs(1, String.valueOf(dictionary[i]));
+            }
+
+            return map.get(word);
+        }
+
+        static void dfs(int depth, String base) {
+            map.put(base, index++);
+            if (depth == 5) {
+                return;
+            }
+            for (int i = 0; i < 5; i++) {
+                dfs(depth + 1, base + dictionary[i]);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.solution("AAAAE"));
+    }
 }
