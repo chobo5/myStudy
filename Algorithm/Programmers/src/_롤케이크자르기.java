1963c8c73684b2a2c8f258aee1ea7b0da522b4d7
JUNYEON WON <wnsdus1008@gmail.com>
2024-10-16 22:44:05 +0900

    롤케이크 자르기

--- Algorithm/Programmers/src/_롤케이크자르기.java
+++ Algorithm/Programmers/src/_롤케이크자르기.java
@@ -6,24 +6,34 @@ public class _롤케이크자르기 {
         public int solution(int[] topping) {
             int answer = 0;
 
-            Map<Integer, Integer> leftMap = new HashMap<>(); // 형
-            Map<Integer, Integer> rightMap = new HashMap<>(); // 동생
+            Map<Integer, Integer> leftMap = new HashMap();
+            Map<Integer, Integer> rightMap = new HashMap();
 
-            for (int e : topping) {
-                rightMap.put(e, rightMap.getOrDefault(e, 0) + 1);
+            leftMap.put(topping[0], 1);
+            for (int i = 1; i < topping.length; i++) {
+                rightMap.put(topping[i], rightMap.getOrDefault(topping[i], 0) + 1);
             }
 
-            for (int e : topping) {
-                leftMap.put(e, leftMap.getOrDefault(e, 0) + 1);
+            if (leftMap.size() == rightMap.size()) {
+                answer++;
+            }
 
-                if (rightMap.get(e) - 1 == 0)
-                    rightMap.remove(e);
-                else
-                    rightMap.put(e, rightMap.get(e) - 1);
+            for (int i = 1; i < topping.length; i++) {
+                leftMap.put(topping[i], leftMap.getOrDefault(topping[i], 0) + 1);
+                Integer value = rightMap.get(topping[i]);
+                if (value == 1) {
+                    rightMap.remove(topping[i]);
+                } else {
+                    rightMap.put(topping[i], value - 1);
+                }
 
-                if (leftMap.size() == rightMap.size())
+                if (leftMap.size() == rightMap.size()) {
                     answer++;
+                }
+
             }
+
+
 
             return answer;
         }
